262526c43c886483ab6d29c90e2b8c6399b9e8da
kimizuka <kimizuka@users.noreply.github.com>
2021-03-12 02:27:28 +0000

    deploy: 21d6eba61cf269d8743707473f80a7ea169a8e37

---
+++ _next/static/zdz_pUknq6qGV55FNpFXU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F0978f8b3ace279608ed4.css","static\u002Fchunks\u002Fpages\u002Findex-72845527b7cfacf965da.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e05ad9a486578767fc1b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
